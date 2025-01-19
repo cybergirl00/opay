be01174ec5a4455253c852e304994b20104b9ce2
HP <dikkorabiat25@gmail.com>
2025-01-19 20:32:44 +0100

    Done with the whole app

--- lib/keys.ts
+++ lib/keys.ts
@@ -1,3 +1,4 @@
 export const flutterwaveKey = process.env.EXPO_PUBLIC_FLUTTERWAVE_KEY;
 export const username = process.env.EXPO_PUBLIC_VTU_USERNAME
 export const password = process.env.EXPO_PUBLIC_VTU_PASSWORD
+export const serverUrl = process.env.EXPO_PUBLIC_SERVER_URL
